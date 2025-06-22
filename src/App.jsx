1de5be8ddade3eb303a5c7d390fb612147756d93
Goncharuk <vtb4055521@corp.dev.vtb>
2025-06-22 20:49:10 +0300

    feature-2: old commits

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,53 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import WebApp from '@twa-dev/sdk';
+
+
+
 function App() {
   const [count, setCount] = useState(0)
 
+  useEffect(() => {
+    WebApp.ready();
+    console.log(WebApp);
+
+    const handleClick = () => {
+      if (!WebApp.isPopupOpened) {
+        WebApp.showAlert("Вы нажали кнопку!");
+      }
+    };
+
+    // Работа с MainButton
+    WebApp.MainButton.setText("Нажми меня");
+    WebApp.MainButton.onClick(handleClick);
+    WebApp.MainButton.show();
+
+    // Работа с BackButton
+    WebApp.BackButton.onClick(() => WebApp.close());
+    WebApp.BackButton.show();
+
+    return () => {
+      WebApp.MainButton.offClick(handleClick);
+      WebApp.BackButton.offClick();
+    };
+  }, []);
+
+  var tg = WebApp.initDataUnsafe;
+
   return (
     <>
-      <h1>ОПАЧКИ</h1>
+      <h1>{tg.user?.first_name}</h1>
       <div className="card">
+        
+        <img src={tg.user?.photo_url} className="logo" alt="photo" />
+
         <button onClick={() => setCount((count) => count + 1)}>
-          Я нажал на кнопку {count} раз
+          Нажал {count} раз
         </button>
+
       </div>
     </>
   )
